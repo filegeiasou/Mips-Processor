091a6d58da5ec3acbc4436daa16ac4f84439cada
Aggelos Mentzelos <filegeiasou@gmail.com>
2023-06-04 15:58:05 +0300

    Update instruction_memory.vhd

--- Project Theory/instruction_memory.vhd
+++ Project Theory/instruction_memory.vhd
@@ -10,7 +10,7 @@ end instrmemory;
 
 
 ARCHITECTURE arch OF instrmemory IS
-type iadd is array(0 to 10) of std_logic_vector(31 downto 0);
+type iadd is array(0 to 15) of std_logic_vector(31 downto 0);
 constant imem: iadd :=(
 "00100000000000000000000000000000",--addi $0 , $0 , 0 okey
 "00100000010000100000000000000000",--addi $2 , $2 , 0 okey
@@ -22,7 +22,11 @@ constant imem: iadd :=(
 "00100000011000110000000000000001",--addi $3 , $3 , 1 okey
 "00100000100001000000000000000001",--addi $4 , $4 , 1 okey
 "00100000101001011111111111111111",--addi $5 , $5 , -1 okey 
-"00010100000001011111111111111010" --bne $5, $0 , okey
+"00010100000001011111111111111010", --bne $5, $0 , okey
+"00000000000000000000000000000000",
+"00000000000000000000000000000000",
+"00000000000000000000000000000000",
+"00000000000000000000000000000000"
 );
 BEGIN
 instr <=imem(to_integer(unsigned(read_addr)));
