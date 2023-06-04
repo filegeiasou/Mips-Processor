76ec23f9084c934dcef3c6ca043e991ff0666779
Aggelos Mentzelos <filegeiasou@gmail.com>
2023-06-04 18:14:04 +0300

    Update addsign.vhd

--- Project Theory/addsign.vhd
+++ Project Theory/addsign.vhd
@@ -3,14 +3,14 @@ use ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.all;
 USE ieee.numeric_std.ALL;
 
-entity addpc is 
+entity addsign is 
 port(
 sign_extd : in std_logic_vector (31 downto 0);
 pc_next1: in std_logic_vector(31 downto 0);
 pc_next2 : out std_logic_vector(31 downto 0));
 end addpc;
 
-architecture dtflow of addpc is
+architecture dtflow of addsign is
 begin
 pc_next2 <= pc_next1 + sign_extd;
 end dtflow;
